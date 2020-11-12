87a3dc70c221ab4bff2f705eab420cf0a129e18d
Rainério <raijunior20@gmail.com>
2020-11-11 21:24:07 -0300

    remove knexfile

--- src/index.js
+++ src/index.js
@@ -8,8 +8,8 @@ const app = express();
 
 app.use(cors())
 app.use(express.json());
-app.use(routes);
+app.use(routes);    
 app.use(errors());
 
 dotenv.config();
-app.listen(3333);
+app.listen(process.env.PORT || 3333);
