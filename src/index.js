5f8d85a27e58f73bd4327db17f465d1206838f31
Rainerio <raijunior20@gmail.com>
2020-11-16 15:14:07 -0300

    backend

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const cors = require('cors')
 const app = express();
 
 app.use(cors({
-    origin: 'https://expo.io/--/to-exp/exp%3A%2F%2Fexp.host%2F%40raineriojr%2FVouchers'
+    origin: 'https://exp.host/@raineriojr/Vouchers'
 }))
 
 app.use(express.json());
