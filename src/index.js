bcf8667eeaed9ffb1a045d97f8f91de46935816e
Rainerio <raijunior20@gmail.com>
2020-11-16 17:13:59 -0300

    backend

--- src/index.js
+++ src/index.js
@@ -8,8 +8,7 @@ const app = express();
 
 app.use(cors({
     origin: ("Access-Control-Allow-Origin", "https://exp.host/@raineriojr/Vouchers"),
-    allowedHeaders: ("X-Total-Count"),
-    exposedHeaders: ("X-Total-Count")
+    exposedHeaders: "X-Total-Count"
 }))
 
 app.use(express.json());
