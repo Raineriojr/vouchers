411111f31ec0c307582ba804475dfdcb4c095aa6
Rainerio <raijunior20@gmail.com>
2020-11-15 22:43:11 -0300

    backend

--- src/index.js
+++ src/index.js
@@ -6,7 +6,10 @@ const cors = require('cors')
 
 const app = express();
 
-app.use(cors())
+app.use(cors({
+    origin: 'https://vouchers-meta-backend.herokuapp.com'
+}))
+
 app.use(express.json());
 app.use(routes);    
 app.use(errors());
