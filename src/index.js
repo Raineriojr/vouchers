77f5800032148472279221feb7c4e24cc563cbcb
Rainerio <raijunior20@gmail.com>
2020-11-17 14:46:50 -0300

    backend

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(cors({
     origin: ("Access-Control-Allow-Origin", "https://exp.host/@raineriojr/Vouchers"),
-    exposedHeaders: ("Access-Control-Expose-Headers", "x-total-count")
+    exposedHeaders: ("Access-Control-Allow-Headers", "x-total-count")
 }))
 
 app.use(express.json());
